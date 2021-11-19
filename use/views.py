503080739c0104ad30934c2232493952be940263
qhsdnr0 <qhsdnr0@gmail.com>
2021-11-20 00:15:07 +0900

    Create : 요금 계산 API 작성
    - 각종 조건에 따른 요금을 확인할 수 있는 API 작성
    
    - 각종 할인 및 벌금 요건은 별도의 utils에서 함수로 관리
    
    - use.urls 추가
    
    Modify : code convention
    
    - 컨벤션 수정

--- use/views.py
+++ use/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+import json
+from datetime       import datetime, timedelta
 
-# Create your views here.
+from django.views   import View
+from django.http    import JsonResponse
+
+from members.models import Member
+
+from .models        import Use, Deer
+from utils          import *
+
+class UseView(View):
+    def post(self, request):
+        try:
+            data      = json.loads(request.body)
+            
+            end_lat   = data['end_lat']
+            end_lng   = data['end_lng']
+            deer_name = data['deer_name']
+            member    = Member.objects.get(id=data["member"])
+            deer      = Deer.objects.select_related('area').get(name=deer_name)
+            rate_plan = deer.area.rate_plan
+            
+            if Use.objects.filter(start_at = data['start_at'], member = member):
+                return JsonResponse({'message' : 'INVALID_ACCESS'}, status = 400)
+            
+            use = Use.objects.create(
+                end_lat   = data['end_lat'],
+                end_lng   = data['end_lng'],
+                start_at  = data['start_at'],
+                end_at    = data['end_at'],
+                deer_name = deer,
+                member    = member
+            )
+            
+            use_time = calculate_time(use.start_at, use.end_at)
+            
+            if use_time <= timedelta(seconds=60):
+                return JsonResponse({'message' : 'CREATED', 'cost' : 0}, status = 201)
+            
+            total_fee = rate_plan.basic + ((use_time.seconds // 60 )) * rate_plan.per_minute
+            
+            if Use.objects.filter(member=member, end_at__lte=convert_time(use.start_at) - timedelta(seconds=1800)).exists():
+                total_fee = ((use_time.seconds // 60 )) * rate_plan.per_minute
+            
+            adjusted_fee = add_fee(deer, member, discount_fee(deer, member, total_fee))
+            
+            return JsonResponse({'message' : 'CREATED', 'cost' : adjusted_fee}, status = 201)
+            
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
---
+++ utils.py
@@ -0,0 +1,47 @@
+import json
+
+from datetime                import datetime
+from django.contrib.gis.geos import Point
+
+from areas.models            import ForbiddenArea, ParkingZone, Area
+from use.models              import Use
+
+def discount_fee(deer, member, total_fee):
+    use            = Use.objects.filter(deer_name=deer, member=member).last()
+    arrive_point   = Point((use.end_lat, use.end_lng))
+    parking_zone   = ParkingZone.objects.get(area=deer.area)
+    parking_point  = Point((parking_zone.center_lat, parking_zone.center_lng))
+    parking_circle = parking_point.buffer(parking_zone.radius)
+    
+    if parking_circle.contains(arrive_point):
+        return total_fee * 0.7
+       
+    return total_fee     
+
+def add_fee(deer, member, total_fee):
+    use          = Use.objects.filter(deer_name=deer, member=member).last()
+    arrive_point = Point((use.end_lat, use.end_lng))
+    surcharge    = 0
+    
+    areas           = Area.objects.all()
+    forbidden_areas = ForbiddenArea.objects.all()
+    
+    for forbidden_area in forbidden_areas:
+        if forbidden_area.boundary.contains(arrive_point):
+            surcharge += 6000
+            break
+       
+    distance = Area.objects.get(deer=deer).boundary.distance(arrive_point) * 8880
+    surcharge += 10 * distance
+    
+    return total_fee + surcharge
+
+def calculate_time(start, end):
+    start_time = datetime.strptime(str(start), "%Y-%m-%d %H:%M:%S")
+    end_time   = datetime.strptime(str(end), "%Y-%m-%d %H:%M:%S")
+    
+    return end_time - start_time
+
+def convert_time(time):
+    
+    return datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
